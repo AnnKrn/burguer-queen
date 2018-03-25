4e63ef4614d3d47ecae385c279f4837e89218d8c
AnnKrn <anakrn.carreon@gmail.com>
2018-03-24 18:24:08 -0600

    uso data con los componentes ya creados

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Title from "./Title.js";
-import {TableHead, TableBody} from "./Table.js";
+import TableBody from "./Table.js";
+import TableHead from "./TableHead.js";
+
 // import Button from "./Buton.js"
 import data from "./data.js"
 
 function App() {
-    return (<div>
+    return (
+    <div>
         <Title/>
         <table>
             <TableHead/>
             <tbody>
                 <TableBody Datos={data.desayuno}/>
+            </tbody>
+        </table>
+        <table>
+            <TableHead/>
+            <tbody>
+                <TableBody Datos={data.commida}/>
             </tbody>
         </table>
     </div>
