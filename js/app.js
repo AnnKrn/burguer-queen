097895d226ade9d91ebefdbfd8226f6857168981
AnnKrn <anakrn.carreon@gmail.com>
2018-03-27 17:48:28 -0600

    pasando a clases los componentes, hacer reutilizable el mismo componente

--- js/app.js
+++ js/app.js
@@ -5,17 +5,31 @@ import TableBebidas from "./bebidas.js";
 import Table from "./TableHead.js";
 import data from "./data.js"
 
-function App(props) {
-    return (
-    <div>
-        <Title/>,
-        <Table  Datos={data.desayuno}/>,
-        <TableBebidas Datos={data.hamburguesa}/>,
-        <Table Datos={data.acompañamientos}/>,
-        <TableBebidas Datos={data.bebidas}/>,
-        <Table Datos={data.ingredienteExtra}/>
-    </div>
-    )
-};
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Title/>,
+                <Table  Datos={data.desayuno}/>,
+                <TableBebidas Datos={data.hamburguesa}/>,
+                <Table Datos={data.acompañamientos}/>,
+                <TableBebidas Datos={data.bebidas}/>,
+                <Table Datos={data.ingredienteExtra}/>
+            </div>
+            )
+    }
+}
+// function App(props) {
+//     return (
+//     <div>
+//         <Title/>,
+//         <Table  Datos={data.desayuno}/>,
+//         <TableBebidas Datos={data.hamburguesa}/>,
+//         <Table Datos={data.acompañamientos}/>,
+//         <TableBebidas Datos={data.bebidas}/>,
+//         <Table Datos={data.ingredienteExtra}/>
+//     </div>
+//     )
+// };
 
 ReactDOM.render(<App />, document.getElementById("react_container"))
--- js/bebidas.js
+++ js/bebidas.js
@@ -1,33 +1,67 @@
 import React from "react";
-import ReactDOM from "react-dom";
-// import TableBody from "./Table.js";
-import data from "./data.js";
 
-export default function TableBebidas(props) {
-    console.log(props)
-    return(
-        <table>
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Grande</th>
-                    <th>Chico</th>
-                </tr>
-            </thead>
-            <tbody>
-            {
-                props.Datos.map(desa => {
+class TableBebidas extends React.Component{
+    constructor(props){
+        super(props)
+        this.item = "Producto"
+        this.big = "Grande"
+        this.small = "Chico"
+    }
+    render(){
+        
+        return(
+            <table>
+                <thead>
+                    <tr>
+                        <th>{this.item}</th>
+                        <th>{this.big}</th>
+                        <th>{this.small}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                this.props.Datos.map((desa, index) => {
                     return(<tr>
-                        <td>{desa.item}</td>
-                        <td>{desa.size.grande}</td>
-                        <td>{desa.size.chico}</td>
+                        <td key={index}>{desa.size.grande}</td>
+                        <td key={index}>{desa.size.chico}</td>
                         <td><button>Agregar</button></td>
                         </tr>
                             ) 
                         })
                     }
+                </tbody>
+            </table>
+        )
+    }
+}
 
-            </tbody>
-        </table>
-    )
-};
+// export default function TableBebidas(props) {
+//     console.log(props)
+//     return(
+//         <table>
+//             <thead>
+//                 <tr>
+//                     <th>Item</th>
+//                     <th>Grande</th>
+//                     <th>Chico</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//             {
+//                 props.Datos.map(desa => {
+//                     return(<tr>
+//                         <td>{desa.item}</td>
+//                         <td>{desa.size.grande}</td>
+//                         <td>{desa.size.chico}</td>
+//                         <td><button>Agregar</button></td>
+//                         </tr>
+//                             ) 
+//                         })
+//                     }
+
+//             </tbody>
+//         </table>
+//     )
+// };
+
+export default TableBebidas
